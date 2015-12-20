a8f0df3d150c005d87353b290b0710b7b401a1db
BenClosner <bdclosne@mtu.edu>
2015-12-19 19:07:07 -0500

    localized all changes to classapp

--- classapp.py
+++ classapp.py
@@ -9,34 +9,48 @@ import sys, os
 class applet:
     def __init__(self):
         self.app = tk.Tk()
-        self.app.title('Packets 4 lyfe')        
+        self.app.title('Packets 4 lyfe')  
+        self.the_int = 0
+        
         
         #slider bar
         self.start = 0 
         self.end = 1000
-        slider = tk.Scale(self.app, from_=self.start, to=self.end, orient=tk.HORIZONTAL)
+        
+        def callback(x):
+            self.the_int = x
+        
+        slider = tk.Scale(self.app, from_=self.start, to=self.end, orient=tk.HORIZONTAL, length=1000, command = lambda x: callback(slider.get()))
         slider.pack(side=tk.BOTTOM, fill=tk.X)
+                
                 
         #packet/keypress bar    
         self.keypressbar = tk.Canvas(self.app, width=1000, height=30, background='white')
         self.keypressbar.pack(side=tk.BOTTOM, fill=tk.X)
 
+
         #menu bar
+        ##exit
         closewindow = lambda: exit()
         menubar = tk.Menu(self.app)
         filemenu = tk.Menu(menubar, tearoff = 0)
         filemenu.add_command(label = "Close", command = closewindow)
         menubar.add_cascade(label = "File", menu = filemenu)
         self.app.config(menu = menubar)
+        ##open pcap
+        filemenu.add_command(label = "Open PCap File", command = self.load_pcap_from_file)
+
 
         #Packet list
         self.listbox = tk.Listbox(self.app, height=30)
         self.listbox.pack(fill=tk.BOTH, expand=1, pady=(0,20))
         self.listbox.insert(tk.END, "i dont really know how to do this part yet lewl.")
 
+
         #update dynamically
         #This needs to be run once before main loop
-        #self.update() 
+        self.update() 
+        
         
         #Start and End time text boxes        
         self.startlabel = tk.Label(self.app, text=str(self.start))
@@ -46,12 +60,12 @@ class applet:
                 #Key pressed at textboxes
                 
         
+    def load_pcap_from_file(self):
+        print("got file {}".format(tk.filedialog.askopenfilename()))   
         
     #Example of how to update dynamically
     def update(self):
-        seconds = int(time.clock())    
-        self.listbox.insert(0, seconds)
-        self.make_line_at(10 * seconds, 'red')
+        self.make_line_at(self.the_int, 'red')
         self.app.after(1000, self.update) #time in ms, func to run
     
     def make_line_at(self, xcoord, text='None', color='green'):
